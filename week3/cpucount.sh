939f6d8c584d5db99fa634cf549706dd26ac3040
epa <epa@localhost.localdomain>
2017-10-05 10:48:33 +0100

    modified file with else statement

--- week3/cpucount.sh
+++ week3/cpucount.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+#searching for the word processor in cpuinfo
+#feed the output of grep into another command, which counts the number of lines
+# wc -l counts the number of lines in its input
+#cnt is assigned the output of grep processor /proc/cpuinfo | wc -l 
+#must include punctuation or command will not run 
 
 cnt=`grep processor /proc/cpuinfo | wc -l`
-
-if [ $cnt -le 2 ]; then
+# if the cnt variable is less than or equal to 2
+if [ $cnt -lt $1 ]; then
 
   echo "Not enough CPUS, exiting"
+else
+  echo "Enough CPUS"
 
 fi
+#closes if statement
 
 
 
