5d815b03cd22c1d2ccdd6fea2470ac6e19369721
epa <epa@localhost.localdomain>
2017-11-16 10:29:09 +0000

    changes made to file wk9

--- week8/loadtestWrapper.sh
+++ week8/loadtestWrapper.sh
@@ -1,6 +1,11 @@
-#!bin/bash
+#!/bin/bash
 
-./loadtest $1
+./loadtest $1 &
+# the & is to background the loadtest
+# process and detach it from the shell
+# fg will return the backgrounded process to the foreground
+
+#mpstat $2
 
 sleep $2
 
